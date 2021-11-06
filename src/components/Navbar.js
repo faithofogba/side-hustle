7741337ba3e5da53289322233fa905381548c1c2
Onome <onomeofogba13@gmail.com>
2021-11-06 21:12:11 +0100

    Add all my files

---
+++ src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components';
+import Profile from "../images/profile.png";
+import {Link} from 'react-router-dom';
+
+
+const Nav = styled.nav`
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   height: 60px;
+   font-size: 14px;
+`;
+const LeftLinks = styled.div`
+   padding: 10px;
+   margin-left: 10px;
+`;
+const RightLinks = styled.div`
+   padding: 10px;
+   margin-right: 16px;
+   display: flex;
+   align-items: center;
+`;
+const NavLinks = styled(Link)`
+   padding: 0 8px;
+   text-decoration: none;
+   color: #000;
+
+   svg {
+   height: 24px;
+   width: 24px;
+}
+`;
+
+const Navbar = () => {
+    return (
+        <Nav>
+            <LeftLinks>
+                <NavLinks to="/">About</NavLinks>
+                <NavLinks to="/">Stores</NavLinks>
+            </LeftLinks>
+            <RightLinks>
+                <NavLinks to="/">Gmail</NavLinks>
+                <NavLinks to="/">Images</NavLinks>
+                <NavLinks to="/">
+                <svg focusable='false' viewBox='0 0 24 24'>
+                    <path d='M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z' />
+               </svg>
+                </NavLinks>
+                <Link to="/">
+                    <img src= {Profile} alt="/"/>
+                </Link>
+            </RightLinks>
+        </Nav>
+    )
+}
+
+export default Navbar
